9069b6b9a69269fd98098e00734192e04fe03e79
Christian Baranowski <develop@pro-pix.de>
2015-02-09 07:48:34 +0100

    cleanup

--- simple.web.blog.web/src/simple/web/blog/web/WebApp.java
+++ simple.web.blog.web/src/simple/web/blog/web/WebApp.java
@@ -9,6 +9,7 @@ import org.glassfish.jersey.server.mvc.Template;
 
 import osgi.web.common.Controller;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.headers.RequireCapability;
 
 @Requieres.Bootstrap
 @Requieres.AngularJS
@@ -21,4 +22,14 @@ public class WebApp implements Controller {
     @Template(name="index.hbs")
     public Object get() { return ""; }
 	
+}
+
+@interface Requieres {
+
+    @RequireCapability(ns = "bootstrap.css", filter = "${frange;3.2.0}")
+    @interface Bootstrap {}
+    
+    @RequireCapability(ns = "angular.js", 	 filter = "${frange;1.2.21}")
+    @interface AngularJS {}
+    
 }

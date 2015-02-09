a80eb68fc0999e86d192f6831b7801915f84defb
Christian Baranowski <develop@pro-pix.de>
2015-02-09 07:12:23 +0100

    add config project and fix some issues

--- simple.web.blog.data/src/simple/web/blog/data/Blog.java
+++ simple.web.blog.data/src/simple/web/blog/data/Blog.java
@@ -1,9 +1,11 @@
 package simple.web.blog.data;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Lob;
 
 import aQute.bnd.annotation.ProviderType;
 
@@ -15,8 +17,10 @@ public class Blog {
     @GeneratedValue(strategy=GenerationType.AUTO) 
     public Long id;
     
+    @Column(length=255)
     public String title;
-    
+
+    @Lob
     public String content;
     
 }

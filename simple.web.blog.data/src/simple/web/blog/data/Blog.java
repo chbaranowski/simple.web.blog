ad1e90a700476657655d378d23105281f68730e3
Christian Baranowski <develop@pro-pix.de>
2015-02-09 07:42:17 +0100

    cleanup the source

--- simple.web.blog.data/src/simple/web/blog/data/Blog.java
+++ simple.web.blog.data/src/simple/web/blog/data/Blog.java
@@ -1,12 +1,6 @@
 package simple.web.blog.data;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Lob;
-
+import javax.persistence.*;
 import aQute.bnd.annotation.ProviderType;
 
 @ProviderType
--- simple.web.blog.data/src/simple/web/blog/data/BlogRepository.java
+++ simple.web.blog.data/src/simple/web/blog/data/BlogRepository.java
@@ -1,9 +1,7 @@
 package simple.web.blog.data;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import aQute.bnd.annotation.ProviderType;
 
 @ProviderType
